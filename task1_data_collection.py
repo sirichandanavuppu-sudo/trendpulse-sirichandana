2088b23c6bc481e4d756795e9aaff65eff7dcf84
Siri Chandana <sirichandana.vuppu@gmail.com>
2026-08-26 16:28:00 +0530

    Import requests, json, os, and time modules
    Added necessary imports for data collection.

--- task1_data_collection.py
+++ task1_data_collection.py
@@ -1,1 +1,7 @@
 # TrendPulse - Task 1: Fetch data from HackerNews API
+import requests
+import json
+import os
+import time
+
+top_stories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
